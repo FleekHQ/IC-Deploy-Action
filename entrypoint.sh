5d5e723d0142b544ae0c1d4cd15cb653c6c68265
gespi <gespi@sylvanas.local>
2022-04-26 21:02:26 -0400

    add npm rsync, and cleanup a bit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,10 @@
 #!/bin/sh -l
 
-echo Creating deployer directory
+# echo Creating deployer directory
 
-mkdir ~/.config
-mkdir ~/.config/dfx
-mkdir ~/.config/dfx/identity
-mkdir ~/.config/dfx/identity/default
+mkdir -p ~/.config/dfx/identity/default
 
-echo Adding identity.pem and wallets file file
+# echo Adding identity.pem and wallets file file
 
 echo $INPUT_IDENTITY > ~/.config/dfx/identity/default/identity.pem
 sed -i 's/\\r\\n/\r\n/g' ~/.config/dfx/identity/default/identity.pem
